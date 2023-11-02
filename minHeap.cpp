0e1814bd1f96f63e95aa43b5e7bb6adb508983b2
userabhishek19 <121710381+userabhishek19@users.noreply.github.com>
2023-11-02 23:50:14 +0530

    Add files via upload

--- minHeap.cpp
+++ minHeap.cpp
@@ -1,84 +1,33 @@
 #include<iostream>
 using namespace std;
 
-class heaps{
-    public:
-    int arr[100];
-    int size;
-    heaps(){
-        arr[0]=-1;
-        size=0;
-}
+void minHaepify(int arr[],int n, int i){
+    int smallest = i;
+    int left = 2*i;
+    int right = 2*i+1;
 
-
-void intsertInMinHeap(int val){
-    size=size+1;
-    int index= size;
-    arr[index]=val;
-
-    while(index<=size){
-        int parent= index/2;
-        if(arr[parent]>arr[index]){
-            swap(arr[index],arr[parent]);
-            index= parent;
-        }
-else{
-    break;
-}
+    if(left <= n && arr[smallest] > arr[left]){
+        smallest = left;
+    }
+    if(right <=n && arr[smallest] > arr[right]){
+        smallest = right;
+    }
+    if(smallest != i){
+        swap(arr[i],arr[smallest]);
+        minHaepify(arr,n,smallest);
     }
 }
-void print(){
-     cout<<"The min heaps are: ";
-    for(int i=1;i<=size;i++){
-       cout<<arr[i]<<" ";
-    }
-    cout<<endl;
-}
-
-void remove(){
-    if(size == 0){
-        cout<<"sir khali hai min heap ";
-        return ;
-    }
-    arr[1]=arr[size];
-
-   int i=1;
-   while(i<size){
-    int leftIndex= 2*i;
-    int rightIndex= 2*i+1;
-    if(leftIndex < size && arr[leftIndex] < arr[i]){
-        swap(arr[leftIndex], arr[i]);
-        size--;
-    }
-    if(rightIndex < size && arr[rightIndex] < arr[i]){
-        swap(arr[rightIndex], arr[i]);
-        size--;
-    }else{
-        return;
-    }
-
-   } 
-
-    
-}
-};
 
 int main(){
-  
-    heaps h;
-    h.intsertInMinHeap(65);
-    h.intsertInMinHeap(30);
-    h.intsertInMinHeap(70);
-    h.intsertInMinHeap(5);
-    h.intsertInMinHeap(3);
-    
-    h.print();
-    h.remove();
-    
+    int arr[6]={-1,50,43,67,32,5};
+    int n=5;
+    for(int i =n/2; i>=1;i--){
+        minHaepify(arr,n,i);
+    }
 
-
-    h.print();
-
-      return 0;
+    cout<<"The minHeapify is: ";
+for(int i=1; i<=n; i++){
+    cout<<arr[i]<< " ";
+}
 
 }
